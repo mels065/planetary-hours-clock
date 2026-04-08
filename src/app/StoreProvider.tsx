00fbaa2e140f80cbc2f93ab9d4ff9a8160eaf59c
Brandon Mellus <mellusbrandon@gmail.com>
2026-04-08 18:55:10 -0400

    Setup redux provider

---
+++ src/app/StoreProvider.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { Provider } from 'react-redux';
+import { makeStore, AppStore } from "../store";
+
+export default function StoreProvider({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    const store = makeStore();
+
+
+    return <Provider store={store}>{children}</Provider>
+}
