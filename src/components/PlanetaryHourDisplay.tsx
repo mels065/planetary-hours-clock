2f7e093bb03fee7316aad9e29afc09e01053baab
Brandon Mellus <mellusbrandon@gmail.com>
2026-04-17 13:08:20 -0400

    Remove unused imports

--- src/components/PlanetaryHourDisplay.tsx
+++ src/components/PlanetaryHourDisplay.tsx
@@ -1,12 +1,6 @@
 "use client"
 
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
 import useSunriseAndSunset from "@/hooks/useSunriseAndSunset";
-import { updateClock, ClockState } from "@/features/clock/clockSlice";
-import { RootState, AppDispatch } from "../store";
-import { renderCurrentDate, renderPlanetaryHour } from "@/utils/utils";
-import { DAYS_OF_WEEK } from "@/utils/constants";
 
 export default function PlanetaryHourDisplay() {
     const { clock } = useSunriseAndSunset();
