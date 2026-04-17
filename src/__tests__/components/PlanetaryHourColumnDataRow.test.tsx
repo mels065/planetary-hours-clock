3ad3b7f43e3445e06c1ecd3a132aeb816922ef1b
Brandon Mellus <mellusbrandon@gmail.com>
2026-04-17 15:43:24 -0400

    Create component for hour row

---
+++ src/__tests__/components/PlanetaryHourColumnDataRow.test.tsx
@@ -0,0 +1,49 @@
+import { describe, expect, test } from "@jest/globals";
+import { render, screen } from '@testing-library/react';
+import PlanetaryHourColumnDataRow from '@/components/PlanetaryHourColumnDataRow';
+import PlanetaryInfo from "@/models/PlanetaryInfo";
+import { Planet } from "@/utils/enums";
+import { PlanetaryHour } from "@/utils/interfaces";
+import '@testing-library/dom';
+
+describe('PlanetaryHourColumnDataRow', () => {
+    const hourString = "Hour of Mercury (☿) / 6:55am - 7:56am";
+    const textGray = 'text-gray';
+    const regexp = /Hour of/;
+
+    const startTime = new Date("March 25, 2026 06:55:00");
+    const endTime = new Date("March 25, 2026 07:56:00");
+    const planetaryHour: PlanetaryHour = {
+        startTime,
+        endTime,
+        planet: PlanetaryInfo.getPlanetaryInfo(Planet.Mercury)
+    };
+
+    test('renders with appropriate text', async () => {
+        render(<PlanetaryHourColumnDataRow 
+            planetaryHour={planetaryHour}
+        />);
+
+        const row = await screen.getByText(regexp);
+        expect(row.textContent).toEqual(hourString);
+    });
+
+    test('does not gray out if isInPast is not present', async () => {
+        render(<PlanetaryHourColumnDataRow
+            planetaryHour={planetaryHour}
+        />)
+
+        const row = await screen.getByText(regexp);
+        expect(row.classList).not.toContain(textGray);
+    })
+
+    test('grays out if time isInPast is passed to it', async () => {
+        render(<PlanetaryHourColumnDataRow
+            planetaryHour={planetaryHour}
+            isInPast={true}
+        />)
+
+        const row = await screen.getByText(regexp);
+        expect(row.classList).toContain(textGray);
+    });
+});
