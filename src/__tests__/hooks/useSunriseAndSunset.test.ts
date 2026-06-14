26e46f88276293c07474aaa6633c11f92c1b2784
Brandon Mellus <mellusbrandon@gmail.com>
2026-06-14 17:46:13 -0400

    Work on test for hook

--- src/__tests__/hooks/useSunriseAndSunset.test.ts
+++ src/__tests__/hooks/useSunriseAndSunset.test.ts
@@ -5,6 +5,8 @@ import { renderHook } from "@testing-library/react";
 import useSunriseAndSunset from "@/hooks/useSunriseAndSunset";
 import * as utils from "../../utils/utils";
 import mockLocalStorage from "@/testutils";
+import { SunriseSunsetApiResponse } from "@/utils/interfaces";
+import { before } from "node:test";
 
 // Source - https://stackoverflow.com/a/76655549
 // Posted by Benjamin Rae
@@ -18,11 +20,48 @@ jest.mock("../../utils/utils.ts", () => {
     return {
     __esModule: true,
     ...jest.requireActual<typeof utils>('../../utils/utils.ts'),
-    isNewDay: () => false,
+    isNewDay: (sunriseTimestamp: string) => new Date(sunriseTimestamp) >= new Date("2026-06-14T05:29:57-04:00"),
   };
 });
 
 describe("useSunriseAndSunset hook", () => {
+    let geolocationMock;
+    before(() => {
+      geolocationMock = jest
+        .spyOn(navigator.geolocation, 'getCurrentPosition')
+        // .mockImplementation()
+    })
+
+    test("it should call the Sunset/Sunrise API if localStorage is empty", () => {
+      const sunrise = "2026-06-13T05:29:57-04:00";
+      const sunset = "2026-06-13T20:32:02-04:00";
+      const tzid = "America/New_York";
+      const status = "OK";
+
+      const apiUrl = ``
+
+      const apiMock = jest
+        .spyOn(axios, 'get')
+        .mockImplementation((url: string) => (
+          Promise.resolve({
+            data: {
+              results: {
+                sunrise: "2026-06-13T05:29:57-04:00",
+                sunset: "2026-06-13T20:32:02-04:00"
+              },
+              status: "OK"
+            }
+          })
+        ));
+
+      const { result } = renderHook(() => useSunriseAndSunset());
+      
+      expect(apiMock.mock.calls.length).toBeGreaterThanOrEqual(1);
+      // expect(apiMock.mock.calls[0][0]).toBe()
+
+      apiMock.mockRestore();
+    });
+
     test("it should return the sunriseTimestamp and sunsetTimestamp in localStorage if timestamps and timezone are set, and is still same day", () => {
         const sunriseTimestamp = "2026-06-13T05:29:57-04:00";
         const sunsetTimestamp = "2026-06-13T20:32:02-04:00";
@@ -36,5 +75,5 @@ describe("useSunriseAndSunset hook", () => {
 
         expect(result.current.sunriseTimestamp).toEqual(sunriseTimestamp);
         expect(result.current.sunsetTimestamp).toEqual(sunsetTimestamp);
-    })
+    });
 })
